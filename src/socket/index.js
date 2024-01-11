cac28effc4476672b03d2fd0dac8ca202eb12498
guneyural <guneyural@gmail.com>
2024-01-12 01:24:49 +0300

    handle socket connection for production and development

--- src/socket/index.js
+++ src/socket/index.js
@@ -3,7 +3,19 @@ import io from "socket.io-client";
 let socket;
 
 function initializeSocket() {
-  socket = io("http://localhost:4000", { transports: ["websocket"] });
+  try {
+    socket = io(
+      process.env.NODE_ENV === "production"
+        ? process.env.REACT_APP_PRODUCTION_SERVER
+        : process.env.REACT_APP_DEVELOPMENT_SERVER,
+      {
+        transports: ["websocket"],
+      }
+    );
+  } catch (err) {
+    console.log("An error occured while initializing socket:");
+    console.log(err);
+  }
 }
 
 export { socket, initializeSocket };
